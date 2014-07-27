057c85a7450417a3a79a73a6f6301d0720ddf4da
Hassan Zaki <hassan.zaki@badrit.com>
2014-07-27 03:58:50 +0200

    * calc after ->(refcator){"looks cool"}

--- func_calc.rb
+++ func_calc.rb
@@ -1,45 +1,16 @@
-def zero(calc=nil)
-  calc.nil? ? 0.0 : calc.(0)
-end
-def one(calc=nil)
-  calc.nil? ? 1.0 : calc.(1)
-end
-def two(calc=nil)
-  calc.nil? ? 2.0 : calc.(2)
-end
-def three(calc=nil)
-  calc.nil? ? 3.0 : calc.(3)
-end
-def four(calc=nil)
-  calc.nil? ? 4.0 : calc.(4) 
-end
-def five(calc=nil)
-  calc.nil? ? 5.0 : calc.(5)
-end
-def six(calc=nil)
-  calc.nil? ? 6.0 : calc.(6)
-end
-def seven(calc=nil)
-  calc.nil? ? 7.0 : calc.(7)
-end
-def eight(calc=nil)
-  calc.nil? ? 8.0 : calc.(8)
-end
-def nine(calc=nil)
-  calc.nil? ? 9.0 : calc.(9)
+num  = [0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0]
+num_s = %w(zero one two three four five six seven eight nine)
+
+num.zip(num_s).each do |n, s|
+  define_method s do |calc=nil|
+    calc.nil? ? n : calc.(n)
+  end
 end
 
-def plus(number)
-  ->(x){ x + number}
-end
-def minus(number)
-  ->(x){ x - number}
-end
-def times(number)
-  ->(x){ x * number}
-end
-def divided_by(number)
-  ->(x){ x / number}
+[[:plus, :+], [:minus, :-], [:times, :*], [:divided_by, :/]].each do |s, c|
+  define_method s do |number|
+    ->(x){ x.send(c, number)}
+  end
 end
 
 require 'minitest/spec'
