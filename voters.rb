fdbd66db2cd39e254482a74d755ba5ebcb65a46f
Hassan Zaki <hassan.zaki@badrit.com>
2014-07-27 03:59:14 +0200

    * fix voters

--- voters.rb
+++ voters.rb
@@ -1,35 +1,20 @@
 def runoff(voters)
-  return nil if voters.empty?
-  return voters[0][0] if voters.length == 1
-  
-  voters_count = voters.max_by{|x| x.length}.length
   voters_rank = {}
-  
-  voters.each do|row|
-    v = row[0] 
-    voters_rank[v] ||= 0
-    voters_rank[v] += 1
-  end
-  
-  # select winners
-  winners = voters_rank.select{|k, v| v == voters_rank.values.max}
-  
-  # all draw
-  return nil if winners.length == voters_count
-  # first wins if 50%+
-  return winners.keys[0] if winners.values[0] >= voters_rank.values.reduce(:+)/2
-  # remove loosers
-  loosers = voters_rank.select{|k, v| v == voters_rank.values.min}
-  #increment next choice
-  loosers.keys.each do |l|
-    voters.each do |row|
-      next unless row[0] == l
-      voters_rank[row[1]] += 1
+  while true
+    voters.each do|row|
+      v = row[0] 
+      voters_rank[v] ||= 0
+      voters_rank[v] += 1
     end
+    
+    winners = voters_rank.select{|k, v| v == voters_rank.values.max}  # select winners
+    return winners.keys[0] if winners.values[0] > voters_rank.values.reduce(:+)/2 # first wins if 50%+
+
+    # remove loosers from voters and voters_rank
+    loosers = voters_rank.select{|k, v| v == voters_rank.values.min}
+    loosers.keys.each{|k| voters_rank.delete(k)}
+    voters.each{|row| loosers.keys.each{|x| row.delete(x)}}
   end
-  voters.each{|row| loosers.keys.each{|x| row.delete(x)}}
-  # run again
-  runoff(voters)
 end
 
 require 'minitest/spec'
@@ -37,17 +22,22 @@ require 'minitest/autorun'
 
 describe "loop_size" do
   it "passes" do
-    voters = [[:dem, :ind, :rep],
-          [:rep, :ind, :dem],
-          [:ind, :dem, :rep],
-          [:ind, :rep, :dem]]
-    runoff(voters).must_equal :ind
+    # voters = [[:dem, :ind, :rep],
+    #           [:rep, :ind, :dem],
+    #           [:ind, :dem, :rep],
+    #           [:ind, :rep, :dem]]
+    # runoff(voters).must_equal :ind
 
+    # runoff([[:dem, :ind, :sam]]).must_equal :dem
 
-    voters = [[:dem, :ind, :sam],
-          [:rep, :ind, :dem],
-          [:sam, :dem, :rep],
-          [:ind, :sam, :dem]]
-    runoff(voters).must_equal :ind
+    # runoff([[:dem, :ind, :sam], 
+    #         [:ind, :dem, :sam]]).must_equal nil
+
+    runoff([[:a, :c, :b, :d, :e], 
+            [:d, :c, :a, :b, :e], 
+            [:e, :b, :d, :a, :c], 
+            [:e, :a, :b, :c, :d], 
+            [:b, :c, :e, :a, :d]]).must_equal :e
+
   end
 end
