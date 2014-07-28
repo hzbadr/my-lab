e8966171444c3bfb0d94fe5069833063a4789f52
Hassan Zaki <hassan.zaki@badrit.com>
2014-07-28 03:45:11 +0200

    * tree node works, let's refactor

---
+++ tree_node.rb
@@ -0,0 +1,50 @@
+class TreeNode
+  attr_accessor :left
+  attr_accessor :right
+  attr_reader :value
+
+  def initialize(left, value, right)
+    @left, @right, @value = left, right, value 
+  end
+
+  def children
+    [@left, @right]
+  end
+end
+
+# return the array containing the tree levels, from root to last level.
+def tree_by_levels(node)
+  nodes = [[node.left, node.right]]
+  nodes.each do |row|
+    break if row.compact.empty?
+    nodes << row.collect{|n| [n.left, n.right] unless n.nil?}.flatten
+  end 
+  [node, *nodes.flatten.compact].map(&:value)
+end
+
+
+tree =  TreeNode.new(
+          TreeNode.new(
+            TreeNode.new(nil, 1, nil),
+            8, 
+            TreeNode.new(nil, 3, nil)
+          ), 
+          2, 
+          TreeNode.new(
+            TreeNode.new(nil, 4, nil),
+            9, 
+            TreeNode.new(nil, 5, nil)
+          )
+        )
+# puts tree
+
+# puts tree_by_levels(tree).inspect
+
+require 'minitest/spec'
+require 'minitest/autorun'
+
+describe "tree_by_levels" do
+  it "passes" do
+    tree_by_levels(tree).must_equal [2,8,9,1,3,4,5]
+  end
+end
