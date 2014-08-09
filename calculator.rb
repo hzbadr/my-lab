acd96b9a443b075b39b1bd9e887fd70c489b8e29
Hassan Zaki <hassan.zaki@badrit.com>
2014-08-09 19:22:55 +0300

    * alot better but .. ?

--- calculator.rb
+++ calculator.rb
@@ -1,29 +1,34 @@
 class Calculator
   # simple calculator parser.
 
+  REGEX = {
+    :/ => /([-+]?[0-9]*\.?[0-9]+\s[\/]\s[-+]?[0-9]*\.?[0-9]+)/,
+    :* => /([-+]?[0-9]*\.?[0-9]+\s[\*]\s[-+]?[0-9]*\.?[0-9]+)/,
+    :- => /([-+]?[0-9]*\.?[0-9]+\s[-]\s[-+]?[0-9]*\.?[0-9]+)/,
+    :+ => /([-+]?[0-9]*\.?[0-9]+\s[\+]\s[-+]?[0-9]*\.?[0-9]+)/
+  }
+
   def evaluate(expr)
     return expr.gsub(/\s[\+|-|\*|\/]\s/, '').to_i unless expr.match /(\d+\s[\*|\/|\+|-]\s\d+)/
 
     @expr = expr
 
-    while @expr.match /(\d+\s[\*|\/]\s\d+)/
-      @expr.scan(/(\d+\s[\*|\/]\s\d+)/) { |match|  calc(match[0]) if match}
-    end
-    
-    while @expr.match /(-?\d+\s[-]\s\d+)/
-      @expr.scan(/(-?\d+\s[-]\s\d+)/) { |match|  calc(match[0]) if match}
-    end
-
-    while @expr.match /(-?\d+\s[+]\s\d+)/
-      @expr.scan(/(-?\d+\s[+]\s\d+)/) { |match|  calc(match[0]) if match}
+    REGEX.keys.each do |operation|
+      reduce(operation)
     end
 
     @expr.to_i
   end
 
+  def reduce(operation)
+    while @expr.match REGEX[operation]
+      @expr.scan(REGEX[operation]) { |match|  calc(match[0]) if match}
+    end
+  end
+
   def calc(match)
     splitted = match.split(' ')
-    @expr = @expr.gsub(match, send(splitted[1], splitted[0].to_i, splitted[2].to_i).to_s)
+    @expr = @expr.gsub(match, send(splitted[1], splitted[0].to_f, splitted[2].to_f).to_s)
   end
 
   %w(/ * + -).each do |operation|
@@ -34,9 +39,11 @@ class Calculator
 
 end
 
-# puts Calculator.new.evaluate("4 / 2")
-# puts Calculator.new.evaluate("2 - 3 - 4")
-puts Calculator.new.evaluate("2 + 3 * 4 / 3 - 6 / 3 * 3 + 8")
+# puts Calculator.new.evaluate("4 / 2") == 2
+# puts Calculator.new.evaluate("2 - 3 - 4") == -5
+# puts Calculator.new.evaluate("10 * 5 / 2") == 25 
+# puts Calculator.new.evaluate("2 + 3 * 4 / 3 - 6 / 3 * 3 + 8") == 8
+puts Calculator.new.evaluate("2 * 1 * 9 * 3 * 6 * 2 * 1 * 1 * 3 * 4") == 7776
 # puts Calculator.new.evaluate("4 + 2")
 # puts Calculator.new.evaluate("2 / 2 + 3 * 4 - 6 * 1 * 1 / 1 + 1 / 1")
 # puts Calculator.new.evaluate("127")
