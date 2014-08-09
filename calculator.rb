737d469eac006da79d40e6fa6cb645a72d961078
Hassan Zaki <hassan.zaki@badrit.com>
2014-08-09 18:23:56 +0300

    * simple calculator parser

--- calculator.rb
+++ calculator.rb
@@ -1,49 +1,44 @@
 class Calculator
+  # simple calculator parser.
 
   def evaluate(expr)
-    return expr.to_i unless expr.include? ' ' 
-    return expr.gsub(/\*|\+\/\-/, ''),to_i if expr.length < 5
-    return send(expr[2], expr[0].to_i, expr[4].to_i) if expr.length <= 5
+    return expr.gsub(/\s[\+|-|\*|\/]\s/, '').to_i unless expr.match /(\d+\s[\*|\/|\+|-]\s\d+)/
 
-    expr = expr.split(/\s?(\+|-)\s?/)
-    xx = expr.collect do |x|
-      if %w(+ -).include? x
-        x
-      else
-        while x.length > 5
-          x = x.gsub(/^(.{5})/, evaluate(x[0..4]).to_s) 
-        end
-        evaluate(x)
-      end
+    @expr = expr
+
+    while @expr.match /(\d+\s[\*|\/]\s\d+)/
+      @expr.scan(/(\d+\s[\*|\/]\s\d+)/) { |match|  calc(match[0]) if match}
+    end
+    
+    while @expr.match /(-?\d+\s[-]\s\d+)/
+      @expr.scan(/(-?\d+\s[-]\s\d+)/) { |match|  calc(match[0]) if match}
     end
 
-    while xx.length >= 3
-      s = send(xx[1], xx[0].to_i, xx[2].to_i)
-      xx = [s] + xx[3..-1]
+    while @expr.match /(-?\d+\s[+]\s\d+)/
+      @expr.scan(/(-?\d+\s[+]\s\d+)/) { |match|  calc(match[0]) if match}
     end
-    xx[0]
+
+    @expr.to_i
   end
 
-  def /(x, y)
-    x/y
+  def calc(match)
+    splitted = match.split(' ')
+    @expr = @expr.gsub(match, send(splitted[1], splitted[0].to_i, splitted[2].to_i).to_s)
   end
 
-  def *(x, y)
-    x*y
-  end
-
-  def +(x, y)
-    x+y
-  end
-
-  def -(x, y)
-    x-y
+  %w(/ * + -).each do |operation|
+    define_method operation do |x, y|
+      x.send(operation, y)
+    end
   end
 
 end
 
 # puts Calculator.new.evaluate("4 / 2")
+# puts Calculator.new.evaluate("2 - 3 - 4")
+puts Calculator.new.evaluate("2 + 3 * 4 / 3 - 6 / 3 * 3 + 8")
+# puts Calculator.new.evaluate("4 + 2")
 # puts Calculator.new.evaluate("2 / 2 + 3 * 4 - 6 * 1 * 1 / 1 + 1 / 1")
 # puts Calculator.new.evaluate("127")
-puts Calculator.new.evaluate("10 * ")
+# puts Calculator.new.evaluate("10 * ")
 # puts Calculator.new.evaluate([1, "+", 12, "-", 6, "+", 1][0..2].join(' '))
