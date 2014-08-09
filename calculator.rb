8126c0afa24c02c8e38419d71d7d4d269b517152
Hassan Zaki <hassan.zaki@badrit.com>
2014-08-09 19:25:08 +0300

    * much much better

--- calculator.rb
+++ calculator.rb
@@ -28,21 +28,15 @@ class Calculator
 
   def calc(match)
     splitted = match.split(' ')
-    @expr = @expr.gsub(match, send(splitted[1], splitted[0].to_f, splitted[2].to_f).to_s)
-  end
-
-  %w(/ * + -).each do |operation|
-    define_method operation do |x, y|
-      x.send(operation, y)
-    end
+    @expr = @expr.sub(match, splitted[0].to_f.send(splitted[1], splitted[2].to_f).to_s)
   end
 
 end
 
-# puts Calculator.new.evaluate("4 / 2") == 2
-# puts Calculator.new.evaluate("2 - 3 - 4") == -5
-# puts Calculator.new.evaluate("10 * 5 / 2") == 25 
-# puts Calculator.new.evaluate("2 + 3 * 4 / 3 - 6 / 3 * 3 + 8") == 8
+puts Calculator.new.evaluate("4 / 2") == 2
+puts Calculator.new.evaluate("2 - 3 - 4") == -5
+puts Calculator.new.evaluate("10 * 5 / 2") == 25 
+puts Calculator.new.evaluate("2 + 3 * 4 / 3 - 6 / 3 * 3 + 8") == 8
 puts Calculator.new.evaluate("2 * 1 * 9 * 3 * 6 * 2 * 1 * 1 * 3 * 4") == 7776
 # puts Calculator.new.evaluate("4 + 2")
 # puts Calculator.new.evaluate("2 / 2 + 3 * 4 - 6 * 1 * 1 / 1 + 1 / 1")
